fa9177762b0adb82b3ada15b14159427eee0b07a
abirjsr <abirjsr21@gmail.com>
2025-07-19 10:06:32 +0600

    hr controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesService } from './employees/employees.service';
 import { EmployeesController } from './employees/employees.controller';
+import { HrService } from './hr/hr.service';
+import { HrController } from './hr/hr.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, EmployeesController],
-  providers: [AppService, EmployeesService],
+  controllers: [AppController, EmployeesController, HrController],
+  providers: [AppService, EmployeesService, HrService],
 })
 export class AppModule {}
