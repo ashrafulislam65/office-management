1bf141d2d6d7843caad9c1d8beb01ae080b28797
Rafsan <sending.to.evan@gmail.com>
2025-08-01 15:11:13 +0600

    Postgress database connected using render platform

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,29 @@ import { EmployeesController } from './employees/employees.controller';
 import { HrController } from './hr/hr.controller';
 import { HrService } from './hr/hr.services';
 import { HrModule } from './hr/hr.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HrModule],
+  imports: [HrModule, TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: 'dpg-d25r49a4d50c73fjma40-a.singapore-postgres.render.com',
+      port: 5432,
+      username: 'office_management_taln_user',
+      password: 'jHyLtss6t2w1s0DWYwMYn25tnmoT0GE2',
+      database: 'office_management_taln',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+      extra : {
+        ssl: {
+          rejectUnauthorized: false,
+        }
+      }
+      } ), 
+],
   controllers: [AppController, EmployeesController, HrController],
   providers: [AppService, EmployeesService, HrService],
 })
 export class AppModule {}
+
